2ab130ba44f87a13288171126a56ddda8fcb0d5f
Jamie Osborne <jmeosbn@gmail.com>
2013-07-21 19:11:50 +0100

    Don't pass (unused) width/height for youtube

--- plugins/custom/youtube.rb
+++ plugins/custom/youtube.rb
@@ -1,15 +1,12 @@
 module Jekyll
   class Youtube < Liquid::Tag
-    @width = 640
-    @height = 390
-
     def initialize(name, id, tokens)
       super
       @id = id
     end
 
     def render(context)
-      %(<iframe width="#{@width}" height="#{@height}" src="http://www.youtube.com/embed/#{@id}" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>)
+      %(<iframe src="http://www.youtube.com/embed/#{@id.strip}" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>)
     end
   end
 end
