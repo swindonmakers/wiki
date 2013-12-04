59ec6b462ac96e04350b1953c68bf71235c49181
Jamie Osborne <jmeosbn@gmail.com>
2013-12-04 21:23:36 +0000

    Fix indentation of module MMD_Meta

--- plugins/custom/mmd_meta/mmd_meta.rb
+++ plugins/custom/mmd_meta/mmd_meta.rb
@@ -17,55 +17,55 @@
 # Note: only calls super if no MultiMarkdown metadata found
 #
 module MMD_Meta
-    #
-    # Read the MultiMarkdown metadata
-    #
-    def read_yaml(base, name)
+  #
+  # Read the MultiMarkdown metadata
+  #
+  def read_yaml(base, name)
 
-      begin
-        self.content = File.read(File.join(base, name))
+    begin
+      self.content = File.read(File.join(base, name))
 
-        # check file uses markdown
-        md = name.match(/\.(markdown|md|mmd)$/) != nil
+      # check file uses markdown
+      md = name.match(/\.(markdown|md|mmd)$/) != nil
 
-        # check for match on MMD metadata
-        if md and self.content =~ /\A([\w-]+:\s*.*?\n)^($\n?)/m
-          self.content = $POSTMATCH
+      # check for match on MMD metadata
+      if md and self.content =~ /\A([\w-]+:\s*.*?\n)^($\n?)/m
+        self.content = $POSTMATCH
 
-          # Metadata keys must start with a letter or number, following
-          #   can be letters, numbers, spaces, hyphens, or underscore.
-          # Metadata keys are case insensitive and stripped of all spaces
-          #   during processing.
-          mmd_meta_var = '[[:alnum:]][\w\- ]+'
+        # Metadata keys must start with a letter or number, following
+        #   can be letters, numbers, spaces, hyphens, or underscore.
+        # Metadata keys are case insensitive and stripped of all spaces
+        #   during processing.
+        mmd_meta_var = '[[:alnum:]][\w\- ]+'
 
-          # convert MMD keys to match Jekyll
-          yaml = $1.gsub(/^#{mmd_meta_var}:/) {
-            # convert keys to lowercase and strip spaces, append space after colon
-            |s| "#{s.downcase.delete ' '} "
-          }
+        # convert MMD keys to match Jekyll
+        yaml = $1.gsub(/^#{mmd_meta_var}:/) {
+          # convert keys to lowercase and strip spaces, append space after colon
+          |s| "#{s.downcase.delete ' '} "
+        }
 
-          # convert MMD variables into liquid tags for page
-          self.content.gsub!(/\[%(#{mmd_meta_var})\]/) {
-            # convert keys to lowercase and strip spaces
-            |s| "{{ page.#{$1.downcase.delete ' '} }}"
-          }
+        # convert MMD variables into liquid tags for page
+        self.content.gsub!(/\[%(#{mmd_meta_var})\]/) {
+          # convert keys to lowercase and strip spaces
+          |s| "{{ page.#{$1.downcase.delete ' '} }}"
+        }
 
-          # pass MMD metadata though (without tabs) as YAML
-          self.data = YAML.load(yaml.gsub(/\t/, ' '))
-        else
-          # pass YAML through to original method if no match
-          super
-          return
-        end
-
-      rescue SyntaxError => e
-        puts "YAML Exception reading #{File.join(base, name)}: #{e.message}"
-      rescue Exception => e
-        puts "Error reading file #{File.join(base, name)}: #{e.message}"
+        # pass MMD metadata though (without tabs) as YAML
+        self.data = YAML.load(yaml.gsub(/\t/, ' '))
+      else
+        # pass YAML through to original method if no match
+        super
+        return
       end
 
-      self.data ||= {}
+    rescue SyntaxError => e
+      puts "YAML Exception reading #{File.join(base, name)}: #{e.message}"
+    rescue Exception => e
+      puts "Error reading file #{File.join(base, name)}: #{e.message}"
     end
+
+    self.data ||= {}
+  end
 
 end
 Jekyll::Post.send(:include, MMD_Meta)
