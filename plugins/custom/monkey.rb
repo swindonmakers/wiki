4674384e2ee49b69de99c8e56814d37d0d40d34c
Jamie Osborne <jmeosbn@gmail.com>
2014-06-02 22:39:00 +0100

    "Fix" post_url tag by matching on slug name only

--- plugins/custom/monkey.rb
+++ plugins/custom/monkey.rb
@@ -11,3 +11,26 @@ module HighlightCode
     div += "<pre><code class='#{lang}'>#{code}</code></pre></div>"
   end
 end
+
+
+#
+# "Fix" non-working post_url tag (date never matches)
+#
+module Jekyll
+  class PostUrl < Liquid::Tag
+    def render(context)
+      site = context.registers[:site]
+
+      site.posts.each do |p|
+        # monkey: only compare the slug name
+        if p.slug == @post.slug
+          return p.url
+        end
+      end
+
+      puts "ERROR: post_url: \"#{@orig_post}\" could not be found"
+
+      return "#"
+    end
+  end
+end
